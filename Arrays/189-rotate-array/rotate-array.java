848462c0fe3675d4a848678970857248e915efee
S.SAMEER BASHA <143730760+sameerthe8055@users.noreply.github.com>
2026-09-21 16:01:00 +0530

    Time: 5 ms (29.30%) | Memory: 268.8 MB (29.09%) - LeetSync

--- Arrays/189-rotate-array/rotate-array.java
+++ Arrays/189-rotate-array/rotate-array.java
@@ -1,9 +1,44 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        if(nums.length == 0) return;
-        rev(nums, 0, nums.length-1);
-        rev(nums, 0, k % nums.length -1);
-        rev(nums, k % nums.length, nums.length-1);
+    public void rotate(int[] arr, int k) {
+        if(arr.length == 0) return;
+        int i, j;
+        //rev(nums, 0, nums.length-1);
+        {
+            i = 0;
+            j = arr.length-1;
+            while(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        // rev(nums, 0, k % nums.length -1);
+        {
+            i = 0;
+            j = k % arr.length -1;
+            while(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        // rev(nums, k % nums.length, nums.length-1);
+        {
+            i = k % arr.length;
+            j = arr.length -1;
+            while(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        
         
     }
     void rev(int[] arr,int i,int j){
